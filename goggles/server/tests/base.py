b644915cad2f2a22ae9fc3d2caafcd6f58889841
Simon de Haan <simon@praekeltfoundation.org>
2014-12-18 22:05:57 +0200

    stubby test for db related non django tests

--- goggles/server/tests/base.py
+++ goggles/server/tests/base.py
@@ -1,5 +1,32 @@
 from twisted.trial.unittest import TestCase
 
+from goggles.service import DBConnection
+
 
 class GoggleTestCase(TestCase):
+
     timeout = 5
+
+    def setup_test_db(self, db_name='goggles'):
+        test_db_name = 'test_tx_%s' % (db_name,)
+        conn = DBConnection()
+        d = conn.connect('dbname=%s' % (db_name,))
+        d.addCallback(lambda _: conn.runOperation("""
+            CREATE DATABASE %s
+            WITH
+                ENCODING = 'UTF8'
+                OWNER = DEFAULT
+                TEMPLATE = template0
+            """ % (test_db_name,)))
+        d.addCallback(lambda _: test_db_name)
+
+        # cleanup db when done
+        self.addCleanup(conn.close)
+        self.addCleanup(
+            conn.runOperation,
+            """
+            DROP DATABASE
+            IF EXISTS
+            %s
+            """ % (test_db_name,))
+        return d
---
+++ goggles/server/tests/test_resource.py
@@ -0,0 +1,7 @@
+from goggles.server.tests.base import GoggleTestCase
+
+
+class TestGoggleResource(GoggleTestCase):
+
+    def test_something(self):
+        return self.setup_test_db()
