2c5c2bdeab5b3d064e8fef47efdc1343a6d41319
Simon de Haan <simon@praekeltfoundation.org>
2015-01-05 11:48:03 +0200

    use latest pip & install treq from github

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ setup(name='goggles',
       scripts=[],
       packages=find_packages(),
       include_package_data=True,
+      dependency_links=[
+          ('https://github.com/dreid/treq/archive/master.tar.gz'
+           '#egg=treq-0.2.1+dev'),
+      ],
       zip_safe=False,
       install_requires=requires,
       tests_require=requires)
